15d45426e5e84e87a6b2d3f3e2f04542b97b8e3b
bharat jagoar <bharatjagoar15@gmail.com>
2024-03-29 21:47:55 +0530

    Let/Const

---
+++ let-const/index.js
@@ -0,0 +1,19 @@
+function letdata(){
+    if(true){
+        var a=12
+        let b=13
+        console.log("inside the if block\n")
+        console.log(a)
+        console.log(b)
+    }
+    console.log("outside the if block\n")
+    console.log(a)
+    // console.log(b)
+}
+
+letdata()
+// console.log(numbers)
+
+//lesson
+// var -> it creates variable that is gonna be accessible within the function and anywhere inside the function 
+//let and const -> it creates a variable that will be only used inside the function only !
